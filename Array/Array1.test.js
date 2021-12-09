4cbf7843f77e0a3c4023713c3f96ab8be82b50cd
MaxSedachev <86675203+MaxSedachev@users.noreply.github.com>
2021-12-09 18:27:08 +0300

    edits in tests for a block array

--- Array/Array1.test.js
+++ Array/Array1.test.js
@@ -1,9 +1,11 @@
-import {twiceValue} from './Array1.js';
+import { twiceValue } from "./Array1";
 
-describe("new array which each element twice as large element of the original array", ()=>{
-    it("return new array[4,8,12,16,20,24,28,32,36,40] with doubled values relative to original array", ()=>{
-        console.log = jest.fn();
-        twiceValue();
-        expect(console.log).toHaveBeenCalledWith([4,8,12,16,20,24,28,32,36,40]);
-    });
+describe("Element is twice the size of the original array", () => {
+  it("Returns a new array [4,8,12,16,20,24,28,32,36,40] x2 values", () => {
+    console.log = jest.fn();
+    twiceValue();
+    expect(console.log).toHaveBeenCalledWith([
+      4, 8, 12, 16, 20, 24, 28, 32, 36, 40,
+    ]);
+  });
 });
