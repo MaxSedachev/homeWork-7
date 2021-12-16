df13f148929f82022c608c00d19121fe08d57aee
MaxSedachev <86675203+MaxSedachev@users.noreply.github.com>
2021-12-17 00:19:57 +0300

    Editing test Date2 for the Date block

--- Date/Date2.test.js
+++ Date/Date2.test.js
@@ -5,6 +5,6 @@ describe("Displaying minutes elapsed since the start of the day", () => {
     const now = new Date(
       "Thu Nov 25 2021 07:34:26 GMT+0300 (Москва, стандартное время)"
     );
-    expect(minPass(now)).toStrictEqual(454);
+    expect(minPass(now)).toBe(274);
   });
 });
